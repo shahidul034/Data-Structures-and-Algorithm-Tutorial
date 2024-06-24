838a8ed70bacd3a16f444a5ac2a82fb65a12d3d4
Shahidul Shakib <31099713+shahidul034@users.noreply.github.com>
2024-06-24 21:24:11 +0600

    array added

---
+++ array.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[]={3,4,5,6,7};
+
+    int arr2[5];
+    for(int i=0;i<5;i++){
+        cout<<"enter: ";
+        cin>>arr2[i];
+    }
+    cout<<arr[0]<<endl;
+
+
+    for(int i=0;i<5;i++){
+        cout<<arr2[i]<<endl;
+    }
+}
